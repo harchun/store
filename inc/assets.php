39576b096a47a6c0ef49e1803916df789e0f2e3f
熊半仙 <z27195322@gmail.com>
2026-09-24 15:44:32 +0800

    feat: expose secure frontend store ajax settings

--- inc/assets.php
+++ inc/assets.php
@@ -4,5 +4,10 @@ function store_enqueue_assets() {
     $v = wp_get_theme()->get('Version');
     wp_enqueue_style('store-style', get_stylesheet_uri(), array(), $v);
     wp_enqueue_script('store-theme', get_template_directory_uri().'/assets/js/store.js', array(), $v, true);
+    wp_localize_script('store-theme','storeTheme',array(
+        'ajaxUrl'=>admin_url('admin-ajax.php'),
+        'searchNonce'=>wp_create_nonce('store_search'),
+        'homeUrl'=>home_url('/')
+    ));
 }
 add_action('wp_enqueue_scripts','store_enqueue_assets');
